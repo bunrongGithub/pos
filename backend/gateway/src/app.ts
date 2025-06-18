1434daa78844c303713b234995e0e9a05a82c76c
Rong <bunbunrong993@gmail.com>
2025-06-18 19:12:39 +0700

    add type axios & modify config routes

--- backend/gateway/src/app.ts
+++ backend/gateway/src/app.ts
@@ -1,8 +1,9 @@
 /// <reference path="./types/express/index.d.ts" />
 
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { authMiddleware } from "./middlewares/auth.middleware";
 import { handleProxy } from "./serivices/proxy";
+import {errorHandlerMiddleware} from "@/src/middlewares/errorHandler.middleware";
 
 const app = express();
 app.use(express.json());
@@ -12,5 +13,7 @@ app.use(authMiddleware, handleProxy);
 app.get("/health", (_req: Request, res: Response) => {
   res.send({ message: "Look Good" });
 });
-
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  errorHandlerMiddleware(err, _req, res, _next);
+});
 export default app;
